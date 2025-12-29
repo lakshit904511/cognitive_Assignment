efc7e7d8e290e0115145d5ee633a155d2edf5a92
lakshit904511 <tyagiparlakshit2k03@gmail.com>
2025-12-29 22:05:25 +0530

    cognitive

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://cognitive-assignment.onrender.com/";
+const BASE_URL = "https://cognitive-assignment.onrender.com";
 
 export async function createPaste(payload) {
   const res = await fetch(`${BASE_URL}/api/pastes`, {
