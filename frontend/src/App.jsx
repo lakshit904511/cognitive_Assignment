a98681ddcb796c7c6de50ba140ac315f6b6040fb
lakshit904511 <tyagiparlakshit2k03@gmail.com>
2025-12-29 22:36:01 +0530

    cognitive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import CreatePaste from "./pages/CreatePage";
 import ViewPaste from "./pages/ViewPaste";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<CreatePaste />} />
         <Route path="/p/:id" element={<ViewPaste />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
